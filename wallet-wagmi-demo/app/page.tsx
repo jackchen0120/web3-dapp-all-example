1319831feaa679d87246bb2ff4e5da0e05110a50
jackchen0120 <278910933@qq.com>
2024-05-09 21:34:37 +0800

    代码优化

--- wallet-wagmi-demo/app/page.tsx
+++ wallet-wagmi-demo/app/page.tsx
@@ -3,20 +3,15 @@
  * @author: Jack Chen @懒人码农
  * @Date: 2024-05-09 00:09:35
  * @LastEditors: Jack Chen
- * @LastEditTime: 2024-05-09 01:05:29
+ * @LastEditTime: 2024-05-09 21:34:09
  */
-import Link from "next/link";
+import WalletConnect from "./wallet-connect/page";
 
 export default function Home() {
   
   return (
     <main className="flex min-h-screen flex-col items-center justify-start">
-    <p>Hi guys, here are the demos:</p>
-    <ol>
-      <li>
-        <Link href="/wallet-connect">连接钱包</Link>
-      </li>
-    </ol>
-  </main>
+      <WalletConnect />
+    </main>
   );
 }
